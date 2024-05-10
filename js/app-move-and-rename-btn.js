f790351581dc8be9556332c5ad3b096c85ab4cc5
a123b1 <155731610+a123b1@users.noreply.github.com>
2024-05-10 12:25:15 +0000

    Update app-move-and-rename-btn.js

--- js/app-move-and-rename-btn.js
+++ js/app-move-and-rename-btn.js
@@ -9659,7 +9659,8 @@ var Rs = Ms,
                     icon: "",
                     tag: "a",
                     href: fileUrl, // t.getFileUrl(e.resourcePath) + `?rootId=${window.props.default_root_id}`,
-                    download: ""
+                    download: "",
+                    title: "下载文件",
                 },
                 on: {
                     click: function(t) {
@@ -9695,6 +9696,7 @@ var Rs = Ms,
                     icon: "",
                     tag: "a",
                     href: "javascript:void(0)",
+                        title: "重命名",
                 },
                 on: {
                     click: function(t) {
@@ -9739,6 +9741,7 @@ var Rs = Ms,
                     icon: "",
                     tag: "a",
                     href: "javascript:void(0)",
+                    title: "移至垃圾桶",
                 },
                 on: {
                     click: function(t) {
@@ -9756,7 +9759,7 @@ var Rs = Ms,
     n("path", {
       attrs: {
         fill: "#F44336",
-        d: "M21.03,3L18,20.31C17.83,21.27 17,22 16,22H8C7,22 6.17,21.27 6,20.31L2.97,3H21.03M5.36,5L8,20H16L18.64,5H5.36M9,18V14H13V18H9M13,13.18L9.82,10L13,6.82L16.18,10L13,13.18Z"
+        d: "M15 13H16.5V15.82L18.94 17.23L18.19 18.53L15 16.69V13M23 16C23 19.87 19.87 23 16 23C14.09 23 12.36 22.24 11.1 21H8C6.9 21 6 20.1 6 19V7H18V9.29C20.89 10.15 23 12.83 23 16M16 11C13.24 11 11 13.24 11 16C11 18.76 13.24 21 16 21C18.76 21 21 18.76 21 16C21 13.24 18.76 11 16 11M19 4V6H5V4H8.5L9.5 3H14.5L15.5 4H19Z"
       }
     })
   ])
@@ -9771,9 +9774,27 @@ var Rs = Ms,
                     icon: "",
                     tag: "a",
                     href: "javascript:void(0)",
+                      title: "永久删除",
                 },
                 on: {
                     click: function(t) {
+                     if (confirm(`永久删除文件${(e.isFolder || e.isGoogleFile)?"夹":""}：“${event.currentTarget.getAttribute('efilename')}”？`)) {
+                        var n = new XMLHttpRequest;
+                        var r = new URL(event.currentTarget.getAttribute('fileurl'));
+                        var params = new URLSearchParams(r.search);
+                        params.set("delete_forever", "true");
+                        params.set("rootId", t.$route.query.rootId || window.props.default_root_id);
+                        r.search = params.toString();
+                        n.onreadystatechange = function() {
+    //                      console.log(n.readyState);
+                          if (n.readyState === 4) {
+                            t.renderPath(t.path, window.props.default_root_id)
+                          }
+                        };
+                        n.open("DELETE", r.href), localStorage.token && n.setRequestHeader("Authorization", "Basic " + localStorage.token), n.send(i)
+
+                }
+                    }
                     }
                 }
             }, [
@@ -9788,7 +9809,7 @@ var Rs = Ms,
     n("path", {
       attrs: {
         fill: "#E50D0D",
-        d: "M21.03,3L18,20.31C17.83,21.27 17,22 16,22H8C7,22 6.17,21.27 6,20.31L2.97,3H21.03M5.36,5L8,20H16L18.64,5H5.36M9,18V14H13V18H9M13,13.18L9.82,10L13,6.82L16.18,10L13,13.18Z"
+        d: "M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8.46,11.88L9.87,10.47L12,12.59L14.12,10.47L15.53,11.88L13.41,14L15.53,16.12L14.12,17.53L12,15.41L9.88,17.53L8.47,16.12L10.59,14L8.46,11.88M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z"
       }
     })
   ])
