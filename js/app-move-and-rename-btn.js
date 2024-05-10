b1eb60caf42ac7273b56135749a7925a23ecc900
a123b1 <155731610+a123b1@users.noreply.github.com>
2024-05-10 12:42:59 +0000

    Update app-move-and-rename-btn.js

--- js/app-move-and-rename-btn.js
+++ js/app-move-and-rename-btn.js
@@ -9686,6 +9686,39 @@ var Rs = Ms,
 
             ], 1)], 1),
 
+            !t.uploadEnabled || e.isFolder || e.isGoogleFile ? t._e() : n("v-list-item-action",
+            {   attrs: {fileurl: fileUrl,efilename: e.fileName}, style: { marginLeft: '8px' },
+                on: {click: function(event) {
+                event.stopPropagation();event.preventDefault();
+
+            }}}, [n("v-btn", { // TODO (e.isFolder || e.isGoogleFile) ? t._e() :
+                attrs: {
+                    icon: "",
+                    tag: "a",
+                    href: "javascript:void(0)",
+                        title: "编辑文档",
+                },
+                on: {
+                    click: function(t) {
+                    }
+                }
+            }, [ n("svg", {
+    attrs: {
+      xmlns: "http://www.w3.org/2000/svg",
+      viewBox: "0 0 24 24",
+      width: "24",
+      height: "24"
+    }
+  }, [
+    n("path", {
+      attrs: {
+        fill: "#E68920",
+        d: "M6,2C4.89,2 4,2.89 4,4V20A2,2 0 0,0 6,22H10V20.09L12.09,18H6V16H14.09L16.09,14H6V12H18.09L20,10.09V8L14,2H6M13,3.5L18.5,9H13V3.5M20.15,13C20,13 19.86,13.05 19.75,13.16L18.73,14.18L20.82,16.26L21.84,15.25C22.05,15.03 22.05,14.67 21.84,14.46L20.54,13.16C20.43,13.05 20.29,13 20.15,13M18.14,14.77L12,20.92V23H14.08L20.23,16.85L18.14,14.77Z"
+      }
+    })
+  ])
+            ], 1)], 1),
+
             !t.uploadEnabled ? t._e() : n("v-list-item-action",
             {   attrs: {fileurl: fileUrl,efilename: e.fileName}, style: { marginLeft: '8px' },
                 on: {click: function(event) {
@@ -9696,7 +9729,7 @@ var Rs = Ms,
                     icon: "",
                     tag: "a",
                     href: "javascript:void(0)",
-                        title: "重命名",
+                        title: "编辑文档",
                 },
                 on: {
                     click: function(t) {
@@ -9777,7 +9810,7 @@ var Rs = Ms,
                       title: "永久删除",
                 },
                 on: {
-                    click: function(t) {
+                    click: function(event) {
                      if (confirm(`永久删除文件${(e.isFolder || e.isGoogleFile)?"夹":""}：“${event.currentTarget.getAttribute('efilename')}”？`)) {
                         var n = new XMLHttpRequest;
                         var r = new URL(event.currentTarget.getAttribute('fileurl'));
