949ed4d02147bd18474c3f274620b5bddbef1c7b
a123b1 <155731610+a123b1@users.noreply.github.com>
2024-05-11 04:50:24 +0000

    Update app-move-and-rename-btn.js

--- js/app-move-and-rename-btn.js
+++ js/app-move-and-rename-btn.js
@@ -9669,26 +9669,26 @@ var Rs = Ms,
                     resourceId: e.resourceId,
                     href: fileUrl, //t.getFileUrl(e.resourcePath) + `?rootId=${window.props.default_root_id}`
                     draggable: "true",
-                    ondragstart: "dragStart(event)",
+//                    ondragstart: "dragStart(event)",
 
                 },
                 on: {
                     click: function(n) {
                         return n.preventDefault(), t.goPath(e.resourcePath, e.opener,e) // TODO
                     },
-//                    dragStart: function(event) {
-//                      console.log("开始拖拽。",t._s(e.fileName));
-//                      event.dataTransfer.setData("text/plain", t._s(e.fileName));
-//                    },
-//                    dragEnd: function(event) {
-//                      console.log("拖拽结束。。",t._s(e.fileName));
-//                    },
-//                    drop: function(event) {
-//                      event.preventDefault();
-//                      const data = event.dataTransfer.getData("text/plain");
-//                      console.log("收到拖拽数据。。。",data);
-//                      event.target.innerText = data;
-//                    }
+                    dragStart: function(event) {
+                      console.log("开始拖拽。",t._s(e.fileName));
+                      event.dataTransfer.setData("text/plain", t._s(e.fileName));
+                    },
+                    dragEnd: function(event) {
+                      console.log("拖拽结束。。",t._s(e.fileName));
+                    },
+                    drop: function(event) {
+                      event.preventDefault();
+                      const data = event.dataTransfer.getData("text/plain");
+                      console.log("收到拖拽数据。。。",data);
+                      event.target.innerText = data;
+                    }
                 }
             }, [
 
